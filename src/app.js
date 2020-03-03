8815070d1e01498faf7c793a5c1b1380c9cd3e66
yunis.hawash <yunis.hawash@gmail.com>
2020-03-03 23:43:13 +0200

    #1 Creating src folder, adding app.js, #2 creating .TODOs file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+// constants
 const express = require('express');
-
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello there');
+// routes
+app.get("/suggestions", (req, res) => {
+  // return data-mocks 
+  res.send({
+    data: {
+      results: [
+        {
+          domain: "awesomefootball.com",
+          available: true
+        },
+        {
+          domain: "gofootball.com",
+          available: false
+        }
+      ]
+    }
+  });
 });
 
+
+// make the app listen to port 7000
 app.listen(7000);
