1773e78537fc2788423896835637c736bcef4266
yunis.hawash <yunis.hawash@gmail.com>
2020-03-04 14:18:16 +0200

    #1 Adding repos , #2 adding resources folder with english tranlsation file, and adding custom translation for the validation of array_min, #3 adding validators folder, and adding key-words-validator

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+// required files
+require("./validators/custom-validation-rules");
+require("./resources/lang/en/validation");
 // constants
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const { Validator } = require("node-input-validator");
+// validators
+const keywordsValidator = require("./validators/keywords-validator");
 
 // using bodyParser to extract the body and get the json data
 app.use(
@@ -15,22 +21,34 @@ app.use(bodyParser.json());
 
 // routes
 app.get("/suggestions", (req, res) => {
+  
+    // create new validator for the keywords
+    let kv = new keywordsValidator(req);
 
-  // return data-mocks 
-  res.send({
-    data: {
-      results: [
-        {
-          domain: "awesomefootball.com",
-          available: true
-        },
-        {
-          domain: "gofootball.com",
-          available: false
-        }
-      ]
-    }
-  });
+    kv.isValid().then((res) => {
+        res.send({
+                    data: {
+                    results: [
+                        {
+                        domain: "awesomefootball.com",
+                        available: true
+                        },
+                        {
+                        domain: "gofootball.com",
+                        available: false
+                        }
+                    ]
+                    }
+                });
+    }).catch((errors)=>{
+        res.status(422).send({
+          errors: kv.getErrors(),
+          meta: {
+            message: "invalid_verification"
+          }
+        });
+    });
+
 });
 
 
---
+++ src/resources/lang/en/validation.js
@@ -0,0 +1,9 @@
+const niv = require("node-input-validator");
+
+// adding custome array size message 
+niv.extendMessages(
+  {
+    array_min: "Required number of :attribute must be :args ."
+  },
+  "en"
+);
